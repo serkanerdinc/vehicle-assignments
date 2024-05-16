7434a1b4278832ee13ee483c30c4d856c53160ce
Serkan Erdinç <srkn.erdinc@gmail.com>
2024-05-16 22:38:29 +0300

    Seeder updated

--- database/seeders/VehicleSeeder.php
+++ database/seeders/VehicleSeeder.php
@@ -16,7 +16,7 @@ class VehicleSeeder extends Seeder
     public function run()
     {
         // Örnek Kullanıcılar
-        DB::table('vehicles')->insert([
+        Vehicle::insert([
             [
                 "plateNumber" => "35BOR001",
                 "model" => "Egea",
