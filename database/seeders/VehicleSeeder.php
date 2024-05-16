5b536d11b890732037c73727bec1fccd18e25cb8
Serkan Erdinç <srkn.erdinc@gmail.com>
2024-05-16 22:13:39 +0300

    Seeder dataları eklendi

---
+++ database/seeders/VehicleSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Vehicle;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class VehicleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Örnek Kullanıcılar
+        Vehicle::create([
+            [
+                "plateNumber" => "35BOR001",
+                "model" => "Egea",
+                "trademark" => "Fiat",
+                "engineNumber" => "123456790"
+            ],
+            [
+                "plateNumber" => "35BOR002",
+                "model" => "Egea",
+                "trademark" => "Fiat",
+                "engineNumber" => "123456791"
+            ],
+            [
+                "plateNumber" => "35BOR003",
+                "model" => "Egea",
+                "trademark" => "Fiat",
+                "engineNumber" => "123456792"
+            ],
+            [
+                "plateNumber" => "35BOR004",
+                "model" => "Egea",
+                "trademark" => "Fiat",
+                "engineNumber" => "123456793"
+            ],
+            [
+                "plateNumber" => "35BOR005",
+                "model" => "Elysee",
+                "trademark" => "Citroen",
+                "engineNumber" => "123456794"
+            ],
+        ]);
+    }
+}
